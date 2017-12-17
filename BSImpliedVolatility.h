8dc3fca6bb8f04e60544b20502fc1b9c3736a708
Daniel Stahl <danstahl1138@gmail.com>
2017-12-17 10:22:46 -0600

    First commit

---
+++ BSImpliedVolatility.h
@@ -0,0 +1,74 @@
+#ifndef __BSIMPLIEDVOLATILITY_H_INCLUDED__
+#define __BSIMPLIEDVOLATILITY_H_INCLUDED__
+#include "BlackScholes.h"
+#include "Newton.h"
+#include "FunctionalUtilities.h"
+#include <cmath>
+
+namespace IV {
+    template<typename S0, typename K, typename Discount, typename Guess, typename Actual>
+    auto getIV(const S0& asset, const K& strike, const Discount& discount, const Actual& callPrice, const Guess& guess){
+        if(asset<0){
+            return -1.0; //something ridiculous
+        }
+        else if(asset-strike>callPrice){
+            return -1.0;//something ridiculous
+        }
+        return newton::zeros(
+            [&](const auto& sigma) {
+                return BSCall(asset, discount, strike, sigma)-callPrice;
+            },
+            guess,
+            .0000001,
+            .0000001,
+            20
+        );
+    }
+
+    template<typename ArrOfAsset, typename ArrOfPrice, typename K, typename Discount, typename Maturity, typename Guess>
+    auto getAllIVByAsset(
+        const ArrOfAsset& assets, 
+        const ArrOfPrice& callPrices, 
+        const K& k, 
+        const Discount& discount, 
+        const Maturity& timeTillMaturity,
+        const Guess& guess
+    ){
+        const Maturity sqrtMat=sqrt(timeTillMaturity);
+        return futilities::for_each_parallel_copy(
+            assets,    
+            [&](
+                const auto& asset, 
+                const auto& index
+            ){
+                return getIV(asset, k, discount, callPrices[index], guess)/sqrtMat;
+            } 
+        );
+    }
+
+    template<typename ArrOfStrike, typename ArrOfPrice, typename S0, typename Discount, typename Maturity, typename Guess>
+    auto getAllIVByStrike(
+        const ArrOfStrike& strikes, 
+        const ArrOfPrice& callPrices, 
+        const S0& asset, 
+        const Discount& discount, 
+        const Maturity& timeTillMaturity,
+        const Guess& guess
+    ){
+        const Maturity sqrtMat=sqrt(timeTillMaturity);
+        return futilities::for_each_parallel_copy(
+            strikes,    
+            [&](
+                const auto& strike, 
+                const auto& index
+            ){
+                return getIV(asset, strike, discount, callPrices[index], guess)/sqrtMat;
+            } 
+        );
+    }
+
+
+}
+
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,72 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "BSImpliedVolatility.h"
+#include "BlackScholes.h"
+
+TEST_CASE("Test IV Alone", "[BSIV]"){
+    const double r=.03;
+    const double S0=50;
+    const double T=1;
+    const double k=50;
+    const double discount=exp(-r*T);
+    const double callPrice=3.0;
+    const double iv=IV::getIV(S0, k, discount, callPrice, .1);
+    std::cout<<"iv: "<<iv<<std::endl;
+    const double callPriceAtVol=BSCall(S0, discount, k, iv);
+    REQUIRE(callPriceAtVol==Approx(callPrice));
+}
+TEST_CASE("Test getAllIVByAsset", "[BSIV]"){
+    const double r=.03;
+    std::vector<double> S0;
+    S0.push_back(40);
+    S0.push_back(50);
+    S0.push_back(60);
+    std::vector<double> callPrice;
+    callPrice.push_back(5);
+    callPrice.push_back(6);
+    callPrice.push_back(15);
+    const double T=1;
+    const double k=50;
+    const double discount=exp(-r*T);
+    
+    const auto ivByAsset=IV::getAllIVByAsset(
+        S0, callPrice, k, discount, T, .5
+    );
+
+    int n=ivByAsset.size();
+    for(int i=0; i<n;++i){
+        std::cout<<ivByAsset[i]<<std::endl;
+    }
+    for(int i=0; i<n;++i){
+        const double callPriceAtVol=BSCall(S0[i], discount, k, ivByAsset[i]*sqrt(T));
+        REQUIRE(callPrice[i]==Approx(callPriceAtVol));
+    }    
+}
+TEST_CASE("Test getAllIVByStrike", "[BSIV]"){
+    const double r=.03;
+    std::vector<double> strike;
+    strike.push_back(40);
+    strike.push_back(50);
+    strike.push_back(60);
+    std::vector<double> callPrice;
+    callPrice.push_back(15);
+    callPrice.push_back(6);
+    callPrice.push_back(5);
+    const double T=1;
+    const double S0=50;
+    const double discount=exp(-r*T);
+    
+    const auto ivByStrike=IV::getAllIVByStrike(
+      strike, callPrice, S0, discount, T, .5
+    );
+    int n=ivByStrike.size();
+    for(int i=0; i<n;++i){
+      std::cout<<ivByStrike[i]<<std::endl;
+    }
+    
+    for(int i=0; i<n;++i){
+        const double callPriceAtVol=BSCall(S0, discount, strike[i], ivByStrike[i]*sqrt(T));
+        REQUIRE(callPrice[i]==Approx(callPriceAtVol));
+    }    
+}
+
