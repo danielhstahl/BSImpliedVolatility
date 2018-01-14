9efbdf55c6cc918c0f21e7dd23b101cb1c39e9f3
Daniel Stahl <daniel.stahl@regions.com>
2018-01-14 12:35:04 -0600

    update api

--- CalibrateOptions.h
+++ CalibrateOptions.h
@@ -6,7 +6,7 @@
 namespace calibrateoptions{
     template<typename OptionCal, typename T, typename U>
     auto l2norm(const std::vector<U>& initArgs, const OptionCal& cal, const std::vector<T>& prices, const std::vector<T>& strikes){
-        return neldermead::Simplex([&](const auto& args){
+        return neldermead::optimize([&](const auto& args){
             return futilities::sum(prices, [&](const auto& val, const auto& index){
                 return futilities::const_power(val-cal(strikes[index], args), 2);
             });
@@ -21,7 +21,7 @@ namespace calibrateoptions{
         const std::vector<T>& prices, 
         const Array& strikes
     ){
-        return neldermead::Simplex([&](const auto& args){
+        return neldermead::optimize([&](const auto& args){
             return constraintFn(args)?5000.0:futilities::sum(cal(strikes, args), [&](const auto& val, const auto& index){
                 return alterCostFn(index)*futilities::const_power(prices[index]-val, 2);
             });
